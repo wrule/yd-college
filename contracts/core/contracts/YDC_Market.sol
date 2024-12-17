ccfa9cfe931111658f73931163d64f2180dab450
jimao <shihao.gu@perfma.com>
2024-12-17 15:20:51 +0800

    feat: Market

--- contracts/core/contracts/YDC_Market.sol
+++ contracts/core/contracts/YDC_Market.sol
@@ -5,7 +5,7 @@ import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 import { Ownable2Step, Ownable } from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import { ERC721URIStorage, ERC721 } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract YDC_Course is ERC721URIStorage, Ownable2Step {
+contract YDC_Market is ERC721URIStorage, Ownable2Step {
   using Strings for uint64;
 
   uint256 private _nextTokenId;

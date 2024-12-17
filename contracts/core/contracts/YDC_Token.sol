4adee08049ac44c8d0727c16432888eaa047a7f4
jimao <shihao.gu@perfma.com>
2024-12-17 14:56:01 +0800

    feat: token Ownable

--- contracts/core/contracts/YDC_Token.sol
+++ contracts/core/contracts/YDC_Token.sol
@@ -1,17 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable2Step, Ownable } from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import { ERC20Votes, ERC20 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 // 1000000000000000000 1YD
 
-contract YDToken is ERC20 {
-  constructor() ERC20("YiDeng Token", "YD") {
+contract YDC_Token is ERC20, Ownable2Step {
+  constructor() ERC20("YiDeng College Token", "YD") Ownable(msg.sender) {
     _mint(msg.sender, 100_000_000 * (10 ** decimals()));
-  }
-
-  // 先开放给任意用户mint
-  function mint(uint16 num) public {
-    _mint(msg.sender, num * (10 ** decimals()));
   }
 }

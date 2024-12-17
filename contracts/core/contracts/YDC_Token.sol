28fbe868ef124c4ceb9978d586857f10e977bde5
jimao <shihao.gu@perfma.com>
2024-12-17 15:05:56 +0800

    feat: mintFor onlyOwner

--- contracts/core/contracts/YDC_Token.sol
+++ contracts/core/contracts/YDC_Token.sol
@@ -4,12 +4,10 @@ pragma solidity ^0.8.28;
 import { Ownable2Step, Ownable } from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import { ERC20Votes, ERC20 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-// 1000000000000000000 1YD
-
 contract YDC_Token is ERC20, Ownable2Step {
   constructor() ERC20("YiDeng College Token", "YD") Ownable(msg.sender) { }
 
-  function mintFor(address user, uint256 amount) public {
+  function mintFor(address user, uint256 amount) public onlyOwner {
     _mint(user, amount);
   }
 

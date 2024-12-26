f42a5446c9b99c55554a2fc98b957378693aa4e0
jimao <shihao.gu@perfma.com>
2024-12-26 15:15:41 +0800

    feat: useContractPost

--- apps/college-ui/src/contracts/useContractPost.ts
+++ apps/college-ui/src/contracts/useContractPost.ts
@@ -2,6 +2,7 @@ import { useMemo } from 'react';
 import { useWeb3React } from '@web3-react/core';
 import { Contract } from 'ethers';
 import { YDC_Post } from '@/typechain-types';
+import YDC_Post_JSON from '@/contracts/abi/YDC_Post.sol/YDC_Post.json';
 
 const useContractPost = () => {
   const { provider, account } = useWeb3React();
@@ -10,10 +11,10 @@ const useContractPost = () => {
     if (!provider) return null;
     return new Contract(
       '0xa1d83b240c81f4C43679B7E7930d12d4A3FF441e',
-      null as any,
-      provider.getSigner(account),
-    );
+      YDC_Post_JSON.abi,
+    ) as unknown as YDC_Post;
   }, [provider, account]);
+
   return [];
 };
 
